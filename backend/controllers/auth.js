1767c483d033d3b5894ca8bb0b7f32a3d14d0b3f
santoschristianjeric <santoschristianjeric@gmail.com>
2024-09-02 07:00:00 +0800

    login, logout, signup

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,14 +1,25 @@
 import User from '../models/User.js';
+import createToken from '../authentication.js'
 import bcryptjs from 'bcryptjs'
+
 
 export const signupUser = async (req, res) => {
     try {
-        const { firstName, lastName, username, password, gender } = req.body;
+        const { firstName, lastName, username, password, confirmPassword, gender } = req.body;
+
+        if (password.length < 8){
+            return res.status(400).send({error: "Password must be 8 characters long"})
+        }
+
+        if (password !== confirmPassword){
+            return res.status(400).send({error: "Password did not match"})
+        }
 
         const duplicateUsername = await User.findOne({ username });
         if (duplicateUsername) {
             return res.status(400).send("duplicate username");
         }
+
         const hashedPassword = bcryptjs.hashSync(password, 10)
 
         let newUser = new User({
@@ -19,22 +30,52 @@ export const signupUser = async (req, res) => {
             gender
         });
 
-        console.log(hashedPassword)
+        if (newUser){
+            createToken(newUser._id, res);
+            await newUser.save();
 
-        await newUser.save();
+            return res.status(201).send({ newUser });
+        } else {
+            return res.status(404).send({error: "Invalid credentials"})
+        }
 
-        return res.status(201).send({ newUser });
+        
 
     } catch (error) {
+        console.log('Error in signup, ', error.message);
         return res.status(500).send({ error: error.message });
     }
 };
 
 
-export const loginUser = (req, res) => {
-    res.send('login')
+export const loginUser = async (req, res) => {
+    try{
+
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcryptjs.compare(password, user?.password || "");
+
+        if (!user || !isPasswordCorrect){
+            return res.status(404).send({error: "Invalid username or password."})
+        }
+
+        createToken(user._id, res);
+
+        return res.status(200).send({user})
+
+    } catch(error){
+        console.log('Error in login, ', error.message)
+        return res.status(500).send({ error: error.message });
+    }
 }
 
 export const logoutUser = (req, res) => {
-    res.send('logout')
+    try {
+        res.cookie("jwt", "", {maxAge: 0});
+        return res.status(200).send({message: "Logout successfully"})
+        
+    } catch (error) {
+        console.log('Error in logout, ', error.message)
+        return res.status(500).send({ error: error.message });
+    }
 }
