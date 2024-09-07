b56ccd5e96ea45cee23b4c0405eb9a3ac1361819
santoschristianjeric <santoschristianjeric@gmail.com>
2024-09-08 03:15:10 +0800

    login & signup pages done

---
+++ frontend/vite-project/src/pages/login/Login.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+            <h1 className='text-3xl font-semibold text-center text-gray-300'>
+                Login 
+                <span> ChatNest</span>
+            </h1>
+
+            <form>
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>Username</span>
+                    </label>
+                    <input type="text" placeholder="Enter username" className="input input-bordered w-full h-10" />
+                </div>
+
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>Password</span>
+                    </label>
+                    <input type="password" placeholder='Enter a password' className='input input-bordered w-full h-10'/>
+                </div>
+
+                <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+                    Don't have an account?
+                </a>
+
+                <div>
+                    <button className='btn btn-block btn-sm mt-2'>Login</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+
+// export default function () {
+//   return (
+//     <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+//         <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+//             <h1 className='text-3xl font-semibold text-center text-gray-300'>
+//                 Login 
+//                 <span> ChatNest</span>
+//             </h1>
+
+//             <form>
+//                 <div>
+//                     <label className='label p-2'>
+//                         <span className='text-base label-text'>Username</span>
+//                     </label>
+//                     <input type="text" placeholder="Enter username" className="input input-bordered w-full h-10" />
+//                 </div>
+
+//                 <div>
+//                     <label className='label p-2'>
+//                         <span className='text-base label-text'>Password</span>
+//                     </label>
+//                     <input type="password" placeholder='Enter a password' className='input input-bordered w-full h-10'/>
+//                 </div>
+
+//                 <a href='#' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+//                     Don't have an account?
+//                 </a>
+
+//                 <div>
+//                     <button className='btn btn-block btn-sm mt-2'>Login</button>
+//                 </div>
+//             </form>
+//         </div>
+
+//     </div>
+//   )
+// }
+
